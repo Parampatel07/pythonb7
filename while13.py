605513e076d8620e9294f1dc7dea353b743982df
parampatel07 <iamparam102@gmail.com>
2023-05-16 19:54:53 +0530

    while

--- while13.py
+++ while13.py
@@ -3,8 +3,8 @@ numbers = [10,22,36,41,50,65,44,59,59,45,10,35,20,65,84,23,1,2,77]
 even = 0
 odd = 0
 count= 0
-even_number =0
-odd_number =0
+even_number = []
+odd_number = []
 # if count <= len(numbers):
 while count < len(numbers):
      if numbers[count] % 2 == 0:
